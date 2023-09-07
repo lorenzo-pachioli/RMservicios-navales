efefe5bb19194e1a9163a611a3f1e886cbb9daa8
lorenzo-pachioli <lorenzopachioli@gmail.com>
2023-09-06 22:13:45 -0300

    Routes fixed

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { InstitucionalComponent } from './components/institucional/institucional.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent,
+    InstitucionalComponent,
+    ServiciosComponent,
+    ContactoComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
