a3205bfcfb4ab4fcc70468b3b0bc0dabca17f20e
lorenzo-pachioli <lorenzopachioli@gmail.com>
2023-09-26 21:27:23 -0300

    Institucional: titule, certificaciones y clientes

--- src/app/routes/components/institucional/institucional.component.ts
+++ src/app/routes/components/institucional/institucional.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstitucionalComponent implements OnInit {
 
+  certificaciones = [
+    "Certificado de Cumplimiento Ambiental Marino. ",
+    "Certificación de Técnicos en Reparación de Motores. ",
+    "Capacitación de motores Yanmar en fábrica de Japón y presentación de cajas reductoras marca Reintjes "
+  ];
+
+  clientes = [
+    "Pedro Moscuzza e hijos",
+    "Grupo agliano",
+    "La Escalerona",
+    "Remolcadores mar del plata",
+    "Transmisiones marinas S.A.",
+    "Grupo mattera",
+    "Grupo makro",
+    "Ciageser S.A",
+    "La Isolana S.R.L."
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
