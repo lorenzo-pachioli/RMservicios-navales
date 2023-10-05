4951548e8728a228309c218d464af9d362caa1f2
lorenzo-pachioli <lorenzopachioli@gmail.com>
2023-10-04 22:11:52 -0300

    scroll back to top button

--- src/app/routes/components/header/header.component.ts
+++ src/app/routes/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   menu = ['inicio', 'institucional', 'servicios', 'contacto'];
-  constructor() { }
+  @ViewChild('header', { static: false }) private header?: ElementRef<HTMLDivElement>;
+  isIntoView: boolean = true;
+
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView() {
+    if (this.header) {
+      const rect = this.header.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      this.isIntoView = topShown && bottomShown;
+    }
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  scrollUp() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    })
+  }
+
+  isInViewport(elem: any) {
+    var distance = elem.getBoundingClientRect();
+    return (
+      distance.top < (window.innerHeight || document.documentElement.clientHeight) && distance.bottom > 0
+    );
   }
 
 }
